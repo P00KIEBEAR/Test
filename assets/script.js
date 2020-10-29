85ea90f8a5fe2aafbf690fd55bceb4d1e78f8fd1
pat paggi <patpaggi@gmail.com>
2020-10-29 09:34:37 -0700

    frist

---
+++ assets/script.js
@@ -0,0 +1,16 @@
+"use strict";
+
+// will need a link 
+/*
+fetch('https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita')
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (json) {
+
+    console.log(json);
+
+  })
+*/
+
+// will need to make new elements
